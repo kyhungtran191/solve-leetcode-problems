88853fa60428fa17ae83ac8b85f7621371c58da5
justinJSwebdev <trankyhung225@gmail.com>
2025-01-27 10:54:15 +0700

    first commit

---
+++ src/array/_1295_Find_Numbers_with_Even_Number_of_Digits.java
@@ -0,0 +1,28 @@
+package array;
+
+public class _1295_Find_Numbers_with_Even_Number_of_Digits {
+    //1295. Find Numbers with Even Number of Digits
+    //https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
+    public int findNumbers(int[] nums) {
+        int sumEvenDigits = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(countDigitNumber(nums[i])%2 == 0){
+                sumEvenDigits++;
+            }
+        }
+        return sumEvenDigits;
+    }
+
+    public int countDigitNumber(int n){
+        int count = 0;
+        while(n>0){
+            count++;
+            n/=10;
+        }
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+    }
+}
